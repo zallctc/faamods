392938db0593de6893de016eded98e1cbd47e22d
zallctc <zallctc@gmail.com>
2024-12-12 16:15:08 +0000

    Add files via upload

---
+++ start/lib/tiktoktop.js
@@ -0,0 +1,293 @@
+const axios = require("axios");
+const fs = require('fs');
+const chalk = require('chalk')
+const cheerio = require('cheerio');
+
+async function tiktok(url) {
+    try {
+        const data = new URLSearchParams({
+            'id': url,
+            'locale': 'id',
+            'tt': 'RFBiZ3Bi'
+        });
+
+        const headers = {
+            'HX-Request': true,
+            'HX-Trigger': '_gcaptcha_pt',
+            'HX-Target': 'target',
+            'HX-Current-URL': 'https://ssstik.io/id',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36',
+            'Referer': 'https://ssstik.io/id'
+        };
+
+        const response = await axios.post('https://ssstik.io/abc?url=dl', data, {
+            headers
+        });
+        const html = response.data;
+
+        const $ = cheerio.load(html);
+
+        const author = $('#avatarAndTextUsual h2').text().trim();
+        const title = $('#avatarAndTextUsual p').text().trim();
+        const video = $('.result_overlay_buttons a.download_link').attr('href');
+        const audio = $('.result_overlay_buttons a.download_link.music').attr('href');
+        const imgLinks = [];
+        $('img[data-splide-lazy]').each((index, element) => {
+            const imgLink = $(element).attr('data-splide-lazy');
+            imgLinks.push(imgLink);
+        });
+
+        const result = {
+            result: {
+            author,
+            title,
+            url: video || imgLinks,  
+            music: audio
+          }
+        };
+        return result
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+}
+
+async function tiktok2(query) {
+  return new Promise(async (resolve, reject) => {
+    try {
+    const encodedParams = new URLSearchParams();
+encodedParams.set('url', query);
+encodedParams.set('hd', '1');
+
+      const response = await axios({
+        method: 'POST',
+        url: 'https://tikwm.com/api/',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'Cookie': 'current_language=en',
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36'
+        },
+        data: encodedParams
+      });
+      const videos = response.data.data;
+        const result = {
+          title: videos.title,
+          cover: videos.cover,
+          origin_cover: videos.origin_cover,
+          no_watermark: videos.play,
+          watermark: videos.wmplay,
+          music: videos.music
+        };
+        resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+async function dlv3(url) {
+    const urls = { url };
+    try {
+        const response = await axios.post('https://snaptikapp.me/wp-json/aio-dl/video-data', urls, {
+            headers: {
+                Accept: '*/*',
+                'Content-Type': 'application/json',
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Mobile Safari/537.36',
+            }
+        });
+        const data = response.data;
+        const result = {
+            data: data,
+        };
+
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.error(error);
+        return error.message;
+    }
+}
+
+
+const clean = (data) => {
+  let regex = /(<([^>]+)>)/gi;
+  data = data.replace(/(<br?\s?\/>)/gi, " \n");
+  return data.replace(regex, "");
+};
+
+async function shortener(url) {
+  return url;
+}
+
+async function dlv4(query) {
+  let response = await axios("https://lovetik.com/api/ajax/search", {
+    method: "POST",
+    data: new URLSearchParams(Object.entries({ query })),
+  });
+
+  result = {};
+
+  result.creator = "YNTKTS";
+  result.title = clean(response.data.desc);
+  result.author = clean(response.data.author);
+  result.nowm = await shortener(
+    (response.data.links[0].a || "").replace("https", "http")
+  );
+  result.watermark = await shortener(
+    (response.data.links[1].a || "").replace("https", "http")
+  );
+  result.audio = await shortener(
+    (response.data.links[2].a || "").replace("https", "http")
+  );
+  result.thumbnail = await shortener(response.data.cover);
+  return result;
+}
+
+async function dlv5(url) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let data = []
+			function formatNumber(integer) {
+				let numb = parseInt(integer)
+				return Number(numb).toLocaleString().replace(/,/g, '.')
+			}
+			//jangan hapus wm, ini hann
+			function formatDate(n, locale = 'id') {
+				let d = new Date(n)
+				return d.toLocaleDateString(locale, {
+					weekday: 'long',
+					day: 'numeric',
+					month: 'long',
+					year: 'numeric',
+					hour: 'numeric',
+					minute: 'numeric',
+					second: 'numeric'
+				})
+			}
+			//jangan hapus wm, ini hannn
+			let domain = 'https://www.tikwm.com/api/';
+			//jangan hapus wm, ini hann
+			let res = await (await axios.post(domain, {}, {
+				headers: {
+					'Accept': 'application/json, text/javascript, */*; q=0.01',
+					'Accept-Language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+					'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+					'Origin': 'https://www.tikwm.com',
+					'Referer': 'https://www.tikwm.com/',
+					'Sec-Ch-Ua': '"Not)A;Brand" ;v="24" , "Chromium" ;v="116"',
+					'Sec-Ch-Ua-Mobile': '?1',
+					'Sec-Ch-Ua-Platform': 'Android',
+					'Sec-Fetch-Dest': 'empty',
+					'Sec-Fetch-Mode': 'cors',
+					'Sec-Fetch-Site': 'same-origin',
+					'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+					'X-Requested-With': 'XMLHttpRequest'
+				},
+				params: {
+					url: url,
+					count: 12,
+					cursor: 0,
+					web: 1,
+					hd: 1
+				}
+			})).data.data
+			//jangan hapus wm, ini han
+			if (res && !res.size && !res.wm_size && !res.hd_size) {
+				res.images.map(v => {
+					data.push({ type: 'photo', url: v })
+				})
+			} else {
+			  //jangan hapus wm, ini hann
+				if (res && res.wmplay) {
+					data.push({ type: 'watermark', url: 'https://www.tikwm.com' + res.wmplay })
+				}
+				//jangan hapus wm, ini hannn
+				if (res && res.play) {
+					data.push({ type: 'nowatermark', url: 'https://www.tikwm.com' + res.play })
+				}
+				//jangan hapus wm, ini hannn
+				if (res && res.hdplay) {
+					data.push({ type: 'nowatermark_hd', url: 'https://www.tikwm.com' + res.hdplay })
+				}
+			}
+			//jangan hapus wm, ini han
+			let json = {
+				status: true,
+				title: res.title,
+				at: formatDate(res.create_time).replace('1970', '').replace(' pukul ',', '),
+				region: res.region,
+				id: res.id,
+				durations: res.duration,
+				duration: res.duration + ' Seconds',
+				cover: 'https://www.tikwm.com' + res.cover,
+				size: {
+				    wm: res.wm_size,
+				    nowm: res.size,
+				    nowm_hd: res.hd_size,
+				},
+				data: data,
+				music_info: {
+					id: res.music_info.id,
+					title: res.music_info.title,
+					author: res.music_info.author,
+					album: res.music_info.album ? res.music_info.album : null,
+					url: 'https://www.tikwm.com' + res.music || res.music_info.play
+				},
+				stats: {
+					views: formatNumber(res.play_count),
+					likes: formatNumber(res.digg_count),
+					comment: formatNumber(res.comment_count),
+					share: formatNumber(res.share_count),
+					download: formatNumber(res.download_count)
+				},
+				author: {
+					id: res.author.id,
+					fullname: res.author.unique_id,
+					nickname: res.author.nickname,
+					avatar: 'https://www.tikwm.com' + res.author.avatar
+				}
+			}
+			resolve(json)
+		} catch (e) {
+			reject(e)
+		}
+	});
+}
+
+async function tiktokStalk(user) {
+  try {
+    const url = await fetch(`https://tiktok.com/@${user}`, {
+      headers: {
+        'User-Agent': 'PostmanRuntime/7.32.2'
+      }
+    });
+    const html = await url.text();
+    const $ = cheerio.load(html);
+    const data = $('#__UNIVERSAL_DATA_FOR_REHYDRATION__').text();
+    const result = JSON.parse(data);
+    if (result['__DEFAULT_SCOPE__']['webapp.user-detail'].statusCode !== 0) {
+      const ress = {
+        status: 'error',
+        message: 'User not found!',
+      };
+      console.log(ress);
+      return ress;
+    }
+    const res = result['__DEFAULT_SCOPE__']['webapp.user-detail']['userInfo'];
+    return res;
+  } catch (err) {
+    console.log(err);
+    return String(err);
+  }
+};
+
+module.exports = { tiktok, tiktok2, dlv3, dlv4, dlv5, tiktokStalk }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
---
+++ start/lib/uploadFile.js
@@ -0,0 +1,72 @@
+const fetch = require('node-fetch');
+const axios = require('axios')
+const FormData = require('form-data');
+const { fromBuffer } = require('file-type');
+
+/**
+ * Upload file to https://pomf.lain.la
+ * @returns {string|null|(string|null)[]}
+ */
+ 
+const pomf = async (buffer) => {
+const { ext, mime } = (await fromBuffer(buffer)) || {};
+  const form = new FormData();
+  form.append("files[]", buffer, { filename: `tmp.${ext}`, contentType: mime });
+  try {
+    const { data } = await axios.post("https://pomf.lain.la/upload.php", form, {
+      headers: form.getHeaders(),
+    });   
+    console.log(data);  
+    return data.files[0].url
+  } catch (error) {
+    throw error;
+  }
+};
+
+/**
+ * Upload epheremal file to file.io
+ * `Expired in 1 day`
+ * `100MB Max Filesize`
+ * @param {Buffer} buffer File Buffer
+ */
+const fileIO = async (buffer) => {
+  const { ext } = await fromBuffer(buffer) || {};
+  const form = new FormData();
+  form.append('file', buffer, `tmp.${ext}`);
+  const res = await fetch('https://file.io/?expires=1d', { // 1 Day Expiry Date
+    method: 'POST',
+    body: form
+  });
+  const json = await res.json();
+  if (!json.success) throw json;
+  return json.link;
+};
+
+/**
+ * Upload file to https://file.btch.rf.gd
+ * @returns {string|null|(string|null)[]}
+ */
+const api = async (buffer) => {
+  let { ext } = await fromBuffer(buffer);
+  let bodyForm = new FormData();
+  bodyForm.append("file", buffer, "file." + ext);
+  let res = await fetch("https://file.btch.rf.gd/api/upload.php", {
+    method: "post",
+    body: bodyForm,
+  });
+  let data = await res.json();
+  let resultUrl = data.result ? data.result.url : '';
+  return resultUrl;
+}
+
+module.exports = async function (inp) {
+  let err = false;
+  for (const upload of [pomf, api, fileIO]) {
+    try {
+      return await upload(inp);
+    } catch (e) {
+      err = e;
+    }
+  }
+  if (err) throw err;
+};
